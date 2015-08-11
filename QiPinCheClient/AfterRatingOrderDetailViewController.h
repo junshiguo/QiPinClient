65327b3434bfa7142d767e39a53b2e943476975a
fdushijia <fdushijia@163.com>
2015-08-11 14:35:01 +0800

    订单详情页面调整

--- QiPinCheClient/AfterRatingOrderDetailViewController.h
+++ QiPinCheClient/AfterRatingOrderDetailViewController.h
@@ -1,28 +1,21 @@
 //
-//  FinishedOrderDetailViewController.h
+//  AfterRatingOrderDetailViewController.h
 //  QiPinCheClient
 //
-//  Created by Shijia on 15/8/10.
+//  Created by Shijia on 15/8/11.
 //  Copyright (c) 2015年 QiPinChe. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "TQStarRatingView.h"
-#import "CommonHeader.h"
 
-@interface FinishedOrderDetailViewController : UIViewController<StarRatingViewDelegate>
+@interface AfterRatingOrderDetailViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UILabel *srcLocation;
 @property (weak, nonatomic) IBOutlet UILabel *desLocation;
 @property (weak, nonatomic) IBOutlet UILabel *startTime;
-
-
-@property (weak, nonatomic) IBOutlet UIButton *nickName;
-@property (weak, nonatomic) IBOutlet UILabel *score;
 @property (weak, nonatomic) IBOutlet UILabel *cash;
 @property (weak, nonatomic) IBOutlet UILabel *cashDescription;
-
-- (IBAction)back:(id)sender;
-- (IBAction)showPartenerDetail:(id)sender;
-
+@property (weak, nonatomic) IBOutlet UIButton *nickName;
+- (IBAction)showPartnerDetail:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *score;
 
 @end
